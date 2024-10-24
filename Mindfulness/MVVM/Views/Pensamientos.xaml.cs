20356121e681f9250cca0446e469c68503ac1b84
Armando Terán Castillo <armandoterancastillo1@outlook.com>
2024-10-24 15:42:24 -0600

    Renombrar proyecto y agregar ventana pensamientos

---
+++ Mindfulness/MVVM/Views/Pensamientos.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Mindfulness.MVVM.Views
+{
+    public partial class Pensamientos : UserControl
+    {
+        public Pensamientos()
+        {
+            InitializeComponent();
+        }
+    }
+}
