95cdcff43b95816a0b6e86da5b2098dcd48b2fdb
Armando Terán Castillo <armandoterancastillo1@outlook.com>
2024-10-24 17:03:08 -0600

    ModalPensamiento Prototipo

---
+++ Mindfulness/MVVM/Views/Controls/ModalPensamiento.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Mindfulness.MVVM.Views.Controls
+{
+    public partial class ModalPensamiento : UserControl
+    {
+        public ModalPensamiento()
+        {
+            InitializeComponent();
+        }
+    }
+}
