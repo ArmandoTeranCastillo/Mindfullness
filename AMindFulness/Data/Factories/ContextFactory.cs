2e4835cec1620ed8a22088029ccb8dcf05e9f5d9
Armando Terán Castillo <armandoterancastillo1@outlook.com>
2024-10-27 19:25:27 -0600

    Create ContextFactory.cs

---
+++ AMindFulness/Data/Factories/ContextFactory.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AMindFulness.Data.Factories
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<Context>
+    {
+        public Context CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<Context> optionsBuilder = new DbContextOptionsBuilder<Context>();
+            optionsBuilder.UseMySql("Server=sql3.freemysqlhosting.net;Database=sql3741179;User ID=sql3741179;Password=nbmBniYtSG;Port=3306;TrustServerCertificate=true;",
+                new MySqlServerVersion(new Version(8, 0, 21)));
+
+            return new Context(optionsBuilder.Options);
+        }
+    }
+}
